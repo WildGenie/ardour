997d1eb34d69108a9c8f4e4274c9461f421eb196
Paul Davis <paul@linuxaudiosystems.com>
2021-07-03 19:03:41 -0600

    fix timeline arithmetic in cue marker motion

--- libs/ardour/region.cc
+++ libs/ardour/region.cc
@@ -1840,18 +1840,18 @@ Region::get_cue_markers (CueMarkers& cues, bool abs) const
 		for (CueMarkers::const_iterator p = x.begin (); p != x.end (); ++p) {
 			if (abs) {
 				cues.insert (*p);
-			} else if (p->position() >= _start && p->position() < _start + _length) {
-				cues.insert (CueMarker (p->text(), p->position() - _start));
+			} else if (p->position() >= start() && p->position() < start() + length()) {
+				cues.insert (CueMarker (p->text(), timepos_t (start().distance (p->position()))));
 			}
 		}
 	}
 }
 
 void
-Region::move_cue_marker (CueMarker const & cm, samplepos_t region_relative_position)
+Region::move_cue_marker (CueMarker const & cm, timepos_t const & region_relative_position)
 {
 	for (SourceList::const_iterator s = _sources.begin (); s != _sources.end(); ++s) {
-		(*s)->move_cue_marker (cm, start() + region_relative_position);
+		(*s)->move_cue_marker (cm, region_relative_position + start());
 	}
 }
 
